cb672c43cbb364fff7d2fd06a2e30b7e24afb064
Brendajacome <bcjacomea@uce.edu.ec>
2024-06-23 22:39:51 -0500

    first commit

---
+++ src/main/java/com/mycompany/walletcash/WalletCash.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.walletcash;
+
+/**
+ *
+ * @author Usuario
+ */
+public class WalletCash {
+
+    public static void main(String[] args) {
+      WalletcashF object = new WalletcashF();
+          object.setVisible(true);
+      
+    }
+}
